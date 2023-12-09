62562e01c2a31b2302ae12ae50c6bf21801b03ae
siddharth9805 <siddharth.u@somaiya.edu>
2023-12-09 01:44:26 -0500

    main tag responsive error resolution

--- component/home.js
+++ component/home.js
@@ -10,8 +10,8 @@ export default function Home(){
                     <source src="/salon.mp4" type="video/mp4" />
             </video>
             <div className={`${"flex flex-column align-items-center justify-content-center justify-item-around"} ${styles.herocontainer}`}>
-                <p className={`${'text-center sm:text-7xl sm:font-bold capitalize mt-0 mb-0'} ${styles.Herotext}`}>Where style meets Precision</p>
-                <p className={`${'text-center sm:text-lg sm:font-bold mb-7'} ${styles.Herotext}`}>Bring a Friend, Beautify Both Your Days - Enjoy Exclusive Rewards Together</p>
+                <p className={`${'text-center text-4xl xl:text-7xl sm:font-bold capitalize mt-0 mb-0'} ${styles.Herotext}`}>Where style meets Precision</p>
+                <p className={`${'text-center sm:text-base sm:font-bold mb-7'} ${styles.Herotext}`}>Share the Beauty, Enjoy the Rewards - Bring a Friend, and Both Shine with a Special Offer</p>
                 <Button label='Book Appointment' className={`${'sm:border-round-lg sm:pt-3 sm:pb-3 sm:pr-7 sm:pl-7 sm:font-semibold mt-7'} ${styles.book_button}`}></Button>
             </div>
         </>
