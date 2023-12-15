ca71d957e2f2faae45af5a365ce7e61bf92db8fa
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-15 14:35:44 -0500

    Updated Content on Home Page. close #30

--- component/home.js
+++ component/home.js
@@ -5,11 +5,11 @@ import { useRouter } from 'next/router';
 
 export default function Home(){
     const header= 'Where style meets Precision'
-    const subheader = 'Share the Beauty, Enjoy the Rewards - Bring a Friend, and Both Shine with a Special Offer';
+    const subheader = `Get Yourself Groomed for The New Year's Eve.... And Ring In 2023 With Unmatched Elegance !!`;
     const router = useRouter();
 
     const redirectToUrl = () => {
-        router.push('/appointment');
+        router.push('/');
       };
     
     return (
@@ -21,7 +21,7 @@ export default function Home(){
                 <div className={`${"flex flex-column align-items-center justify-content-center h-screen"}`}>
                     <p className={`${'text-center text-4xl xl:text-7xl sm:font-bold capitalize mt-0 mb-0'} ${styles.Herotext}`}>{header}</p>
                     <p className={`${'text-center sm:text-base sm:font-bold mb-7'} ${styles.Herotext}`}>{subheader}</p>
-                    <Button label='Book Appointment' className={`${'sm:border-round-lg sm:pt-3 sm:pb-3 sm:pr-7 sm:pl-7 sm:font-semibold mt-7'} ${styles.book_button}`} onClick={redirectToUrl}></Button>
+                    <Button label='View Offers' className={`${'sm:border-round-lg sm:pt-3 sm:pb-3 sm:pr-7 sm:pl-7 sm:font-semibold mt-7'} ${styles.book_button}`} onClick={redirectToUrl}></Button>
                 </div>
             </div>
         </>
