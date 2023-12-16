292ed6d8e4088697eade667e5e652cfb51158856
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-15 19:01:52 -0500

    Added header content in About Page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { useRouter } from 'next/router';
 import Nav from '@/component/nav';
 import Footer from '@/component/footer';
-import styles from '@/styles/utils.module.css'
+import styles from '@/styles/about.module.css';
 
 export default function About() {
     const homeRef = useRef(null);
@@ -34,6 +34,26 @@ export default function About() {
             <div className='overflow-x-hidden'>
                 <div className={styles.backgroundFrame}>
                     <Nav onMenuClick={onMenuClick} />
+                </div>
+                <div className={styles.aboutContainer}>
+                    <div className={styles.leftSection}>
+                        <div className={styles.headerContent}>
+                            <p>Hello ! Welcome to</p>
+                            <h1>Razor Edge</h1>
+                            <p>Where your hair journey meets our masterful artistry !!</p>
+                            <p>Embracing the hero within yourself, we transform styles with precision and flair.  Here, each snip and style is an epic adventure, crafting bold, empowering looks.  Join us, and unleash the hero within through every cut & colour.</p>
+                        </div>
+                        <div className={styles.storyContainer}>
+                            <div className={styles.storyImage}></div>
+                            <div className={styles.storyContent}></div>
+                        </div>
+                        <div className={styles.visionContainer}>
+                            <div className={styles.visionContent}></div>
+                            <div className={styles.visionImage}></div>
+                        </div>
+                    </div>
+                    <div className={styles.rightSection}></div>
+
                 </div>
                 <div ref={contactUsRef}><Footer/></div>
             </div>
