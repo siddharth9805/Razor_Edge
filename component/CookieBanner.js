360153ec60a6e8d56726ac55f6d1fc00541e46e5
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-17 04:25:12 -0500

    Privacy Policy Modal created. close #28

--- component/CookieBanner.js
+++ component/CookieBanner.js
@@ -1,4 +1,5 @@
 import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
 import { useState, useEffect } from 'react';
 import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
 import styles from './css/cookiebanner.module.css';
@@ -7,6 +8,7 @@ import styles from './css/cookiebanner.module.css';
 export default function CookieBanner(){
     const [loading, setLoading] = useState(false);
     const [cookieConsent, setCookieConsent] = useState(false);
+    const [visible, setVisible] = useState(false);
 
     const load = () => {
         setLoading(true);
@@ -40,12 +42,31 @@ export default function CookieBanner(){
 
     return (
         // Restructuring Cookie Banner and its Content as per Team Suggestions
-        <div className={`${`${cookieConsent ? "hidden" : "flex"} justify-content-center align-items-center flex-column w-full fixed left-0 right-0`}  ${styles.CBContainer}`}>
+        <div className={`${`${cookieConsent ? "hidden" : "flex"} justify-content-center align-items-center bg-black-alpha-90 opacity-80 flex-column w-full fixed left-0 right-0`}  ${styles.CBContainer}`}>
             <p id='CBContent'>This site uses cookies to measure and improve your performance.</p>
-            <div id='CBButtonContainer' className={`${'flex justify-content-around w-4'} ${styles.CBButtons}`}>
-                <Button id='CB_decline_button1' label="Opt-Out" icon="" onClick={() => setCookieConsent(false)} />
-                <Button id='CB_detail_button2' label="Details" icon="" />
-                <Button id='CB_accept_button3' label="Accept" loading={loading} onClick={() => setCookieConsent(true)} />
+            <div className='flex flex-column sm:flex-row justify-content-between w-4 '>
+                <div className={'flex flex-row flex-wrap'}>
+                    <Button className={` ${styles.CBLeftButton} flex align-items-center justify-content-center font-bold bg-black-alpha-90 text-white border-white border-round-2xl m-1 sm:m-2`} id='CB_decline_button1' label="Opt-Out" icon="" onClick={() => setCookieConsent(false)} />
+                    <Button className={` ${styles.CBLeftButton} flex align-items-center justify-content-center font-bold bg-black-alpha-90 text-white border-white border-round-2xl m-1 sm:m-2`} id='CB_detail_button2' label="Details" icon="pi pi-external-link" onClick={() => setVisible(true)}/>
+                </div>
+                        
+                <div>
+                    <Dialog header="COOKIE PREFERENCES" visible={visible} onHide={() => setVisible(false)}
+                        style={{ width: '50vw' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
+                        <p className="m-0">
+                            <br/>
+                            <strong>1. Introduction</strong> 
+                            <br/>
+                            Welcome to Razors Edge Salon! By accessing our website and using our services, you agree to comply with and be bound by the following terms and conditions. If you disagree with any part of these terms, please do not use our website. <br/>
+                            <br/>
+                            <strong>2. Use of the Website</strong>
+                            <br/>
+                            The content of this website is for your general information and use only. It is subject to change without notice. Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offense. <br/>
+                            <br/>
+                        </p>
+                    </Dialog>
+                    <Button className={`flex align-items-center justify-content-center font-bold border-round-2xl m-1  sm:m-2`} id='CB_accept_button3' label="Accept" loading={loading} onClick={() => setCookieConsent(true)} />
+                </div>
             </div>
         </div>
     )}
