f65077448b26d44ba6f8ec4d3955f3699e8b9bfd
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-17 00:12:45 -0500

    added content for Cookie Banner. close #27

--- component/CookieBanner.js
+++ component/CookieBanner.js
@@ -39,23 +39,12 @@ export default function CookieBanner(){
     }, [cookieConsent]);
 
     return (
-        <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
-                        fixed ${cookieConsent ? "hidden" : "flex"} bottom-0 left-0 right-0 
-                          px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-700 rounded-lg shadow`}>
-
-            <div className='text-center'>
-                {/* <Link href="/info/cookies"><p>We use <span className='font-bold text-sky-400'>cookies</span> on our site.</p></Link> */}
-                <p className={styles.CBContent}>This site uses cookies to measure and improve your performance.</p>
-                <div className={`justify-content-center flex gap-5 ${styles.CBButtons}`}>
-                    <Button label="Opt-Out" icon="" onClick={() => setCookieConsent(false)} />
-                    <Button label="Details" icon="" />
-                    <Button label="Accept" loading={loading} onClick={load} />
-                </div>
+        <div className={`${`${cookieConsent ? "hidden" : "flex"} justify-content-center align-items-center flex-column w-full fixed left-0 right-0`}  ${styles.CBContainer}`}>
+            <p>This site uses cookies to measure and improve your performance.</p>
+            <div className={`${'flex justify-content-around w-4'} ${styles.CBButtons}`}>
+                <Button label="Opt-Out" icon="" onClick={() => setCookieConsent(false)} />
+                <Button label="Details" icon="" />
+                <Button label="Accept" loading={loading} onClick={() => setCookieConsent(true)} />
             </div>
-            {/* <div className='flex gap-2'>
-                <button className='px-5 py-2 text-gray-300 rounded-md border-gray-900' onClick={() => setCookieConsent(false)}>Decline</button>
-                <button className='bg-gray-900 px-5 py-2 text-white rounded-lg' onClick={() => setCookieConsent(true)}>Allow Cookies</button>
-            </div>    */}
         </div>
     )}
