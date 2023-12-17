f107730c9a9d2a4d1a7c192f80ad61eaf962dad1
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-17 04:29:14 -0500

    added few more element ids for uniqueness.

--- pages/about.js
+++ pages/about.js
@@ -36,40 +36,40 @@ export default function About() {
                 <div className={styles.backgroundFrame}>
                     <Nav onMenuClick={onMenuClick} />
                 </div>
-                <div className={styles.aboutContainer}>
-                    <div className={styles.leftSection}>
-                        <div className={styles.headerContent}>
-                            <p>Hello ! Welcome to</p>
-                            <h1>Razor Edge</h1>
-                            <p>Where your hair journey meets our masterful artistry !!</p>
-                            <p>Embracing the hero within yourself, we transform styles with precision and flair.  Here, each snip and style is an epic adventure, crafting bold, empowering looks.  Join us, and unleash the hero within through every cut & colour.</p>
+                <div id='AboutPageContainer' className={styles.aboutContainer}>
+                    <div id='leftSectionContainer' className={styles.leftSection}>
+                        <div id='leftHeaderContainer' className={styles.leftheaderContentContainer}>
+                            <p id='aboutTitle'>Hello ! Welcome to</p>
+                            <h1 id='aboutSiteName'>Razor Edge</h1>
+                            <p id='aboutContent'>Where your hair journey meets our masterful artistry !!</p>
+                            <p id='aboutContent'>Embracing the hero within yourself, we transform styles with precision and flair.  Here, each snip and style is an epic adventure, crafting bold, empowering looks.  Join us, and unleash the hero within through every cut & colour.</p>
                         </div>
-                        <div className={styles.storyContainer}>
-                            <div className={styles.storyImage}>
-                                <Image src='/public/story-img' alt='story-img' className=''/>
+                        <div id='storySectionContainer' className={styles.storyContainer}>
+                            <div id='storyImage' className={styles.storyImage}>
+                                <Image id='storyRelatedImage' src='/public/story-img' alt='story-img' className=''/>
                             </div>
-                            <div className={styles.storyContent}>
-                                <p className={styles.storyHeading}>Our Story</p>
-                                <p className={styles.storySubHeading}>Nurturing Beauty, Empowering Confidence</p>
-                                <p className={styles.storyContent}>From humble beginnings to becoming a haven for beauty enthusiasts, our story is woven with passion, dedication, and a commitment to empowering individuals through self-expression.</p>
+                            <div id='storyContentContainer' className={styles.storyContent}>
+                                <p id='storyTitle' className={styles.storyHeading}>Our Story</p>
+                                <p id='storySubTitle' className={styles.storySubHeading}>Nurturing Beauty, Empowering Confidence</p>
+                                <p id='storyContext' className={styles.storyContent}>From humble beginnings to becoming a haven for beauty enthusiasts, our story is woven with passion, dedication, and a commitment to empowering individuals through self-expression.</p>
                             </div>
                         </div>
-                        <div className={styles.visionContainer}>
-                            <div className={styles.visionContent}>
-                                <p className={styles.visionHeading}>Our Vision</p>
-                                <p className={styles.visionSubHeading}>Elevating Beauty Beyond Asthetics</p>
-                                <p className={styles.visionContent}>We believe in transformative experiences that go beyond aesthetics, fostering self-love and confidence. Our commitment is to redefine beauty standards and inspire individuals to embrace their uniqueness.</p>
+                        <div id='visionSectionContainer' className={styles.visionContainer}>
+                            <div id='visionContentContainer' className={styles.visionContent}>
+                                <p id='visionTitle' className={styles.visionHeading}>Our Vision</p>
+                                <p id='visionSubTitle' className={styles.visionSubHeading}>Elevating Beauty Beyond Asthetics</p>
+                                <p id='visionContext' className={styles.visionContent}>We believe in transformative experiences that go beyond aesthetics, fostering self-love and confidence. Our commitment is to redefine beauty standards and inspire individuals to embrace their uniqueness.</p>
                             </div>
-                            <div className={styles.visionImage}>
-                                <Image src='/public/vision-img' alt='vision-img' className=''/>
+                            <div id='visionImage' className={styles.visionImage}>
+                                <Image id='visionRelatedImage' src='/public/vision-img' alt='vision-img' className=''/>
                             </div>
                         </div>
                     </div>
-                    <div className={styles.rightSection}>
-                        <Image src='/public/about-img' alt='about-img' className=''/>
+                    <div id='rightSectionContainer' className={styles.rightSection}>
+                        <Image id='rightSectionImage' src='/public/about-img' alt='about-img' className=''/>
                     </div>
                 </div>
-                <div ref={contactUsRef}><Footer/></div>
+                <div id='footerSection' ref={contactUsRef}><Footer/></div>
             </div>
         </>
     )
