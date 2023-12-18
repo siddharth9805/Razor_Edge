27eb8a664f2af6b32e39fae6fc81d555a4b6e9c2
Shrey Trivedi <shreytrivedi577@gmail.com>
2023-12-18 02:57:04 -0500

    Integrated Mailchimp + Toast Messages for Banner

--- component/home.js
+++ component/home.js
@@ -2,15 +2,69 @@ import styles from './css/home.module.css'
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from "primereact/inputtext";
-import { classNames } from 'primereact/utils';
-import React, { useState } from 'react';
+import { Toast } from 'primereact/toast';
+import React, { useRef, useState } from 'react';
 
 export default function Home(){
     const header= 'Where style meets Precision'
-    const subheader = `Get Yourself Groomed for The New Year's Eve.... And Ring In 2023 With Unmatched Elegance !!`;
+    const subheader = `Get Yourself Groomed for The New Year's Eve.... And Ring In 2024 With Unmatched Elegance !!`;
     
     // useState for using Dialog
     const [visible, setVisible] = useState(false);
+    
+    // useState for Email
+    const [email, setEmail] = useState('');
+    const toast = useRef(null);
+
+    // For Loading Effect on Button
+    const [loading, setLoading] = useState(false);
+    const load = () => {
+        setLoading(true);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    };
+
+    // Validating Email and comparing Regex
+    const validateEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    }
+
+    const getemailvalue = (e)=>{
+        setEmail(e.target.value);
+    }
+
+    const Mailchimp = async () => {
+        const result = validateEmail(email);
+        if (result) {
+          const formData = new FormData();
+          formData.append('EMAIL', email);
+          formData.append('b_b08d6dbad505ad420fd015fa5_d0b78b3b60', '');
+      
+          const mailchimpUrl = 'https://app.us21.list-manage.com/subscribe/post?u=b08d6dbad505ad420fd015fa5&id=d0b78b3b60&f_id=00d4e6e6f0';
+      
+          try {
+            const response = await fetch(mailchimpUrl, {
+              method: 'POST',
+              body: formData,
+              mode: 'no-cors', // keep this to avoid CORS issues
+            });
+            toast.current.show({severity:'success', summary: 'Success', detail: 'Subscribed to RE Succesfully', life: 3000});
+            setEmail('')
+          } catch (error) {
+            toast.current.show({severity:'error', summary: 'Error', detail: 'Subscription failed. Try Again!', life: 3000});
+          }
+        } else {
+          toast.current.show({severity:'error', summary: 'Error', detail: 'Invalid Email Provided', life: 3000});
+        }
+      };
+
+      const handleButtonClick = () => {
+        Mailchimp();
+        load();
+      }
     
     return (
         <>
@@ -18,6 +72,7 @@ export default function Home(){
                 <video autoPlay muted loop id="backgroundVideo" className={`${styles.backgroundVideo}`}>
                         <source src="/salon.mp4" type="video/mp4" />
                 </video>
+                <Toast ref={toast} />
                 <div className={`${"flex flex-column align-items-center justify-content-center h-screen"}`}>
                     <p id='heroTitleText' className={`${'text-center text-4xl xl:text-7xl sm:font-bold capitalize mt-0 mb-0'} ${styles.Herotext}`}>{header}</p>
                     <p id='heroSubTitleText' className={`${'text-center sm:text-base sm:font-bold mb-7'} ${styles.Herotext}`}>{subheader}</p>
@@ -41,12 +96,12 @@ export default function Home(){
                             </div>
                             <div className={`${styles.offerBackground} flex flex-column align-items-center justify-content-center h-auto`}>
                                 <div id='offerBackgroundImg' className={`text-center m-1 border-round-md h-6 ${styles.offerText}`} />
-                                <p id='offerSubscribeText' className='text-center text-yellow-200 text-2xl mt-5'>SUBSCRIBE TO OUR NEWSLETTER</p>
+                                <p id='offerSubscribeText' className='text-center text-yellow-200 text-2xl mt-5 z-1'>SUBSCRIBE TO OUR NEWSLETTER</p>
                                 <div className='m-2 flex flex-column align-items-center justify-content-around h-6rem'>
-                                    <InputText id='offerInputEmail' label="Email Address" placeholder='Enter your Email Address' className='z-1 text-black-alpha-80' />
-                                    <Button id='offerEmailSubmit' label='Submit' className='' />
+                                    <InputText id='offerInputEmail' label="Email Address" value={email} placeholder='Enter your Email Address' onChange={getemailvalue} className='z-1 text-black-alpha-80' />
+                                    <Button id='offerEmailSubmit' label='Submit' loading={loading} className='' onClick={handleButtonClick} />
                                 </div>
-                                <p id='likingOfferText' className={`text-center text-white font-medium text-lg mt-5 ${styles.offerText}`}>THE FIRST 100 CUSTOMERS WILL <br/>RECEIVE A COMPLIMENTARY BEARD TRIM!</p>                                        
+                                <p id='likingOfferText' className={`text-center text-white font-medium text-lg mt-5 z-1 ${styles.offerText}`}>THE FIRST 100 CUSTOMERS WILL <br/>RECEIVE A COMPLIMENTARY BEARD TRIM!</p>                                        
                             </div>
                         </div>
                     </Dialog>
