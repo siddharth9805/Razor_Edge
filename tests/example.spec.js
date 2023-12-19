b1417ed0523bfc0478b8146bbbe0bfd54c6c4a6e
Nachiket <67835337+Nachiket-cmd@users.noreply.github.com>
2023-12-19 01:05:12 -0500

    Close #30

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -13,3 +13,16 @@ test('Check the title of Next.js website', async ({ page }) => {
   // Assert that the title is as expected
   expect(pageTitle).toBe(websiteTitle);
 });
+
+
+test('Check if the hero section loads properly on the Next.js website', async ({ page }) => {
+  // Navigate to your Next.js website
+  await page.goto('http://localhost:3000/');
+
+  // Wait for the hero section to be visible. Replace 'your-hero-section-selector' with the actual CSS selector for your hero section.
+  await page.waitForSelector('#home');
+
+  // Check if the hero section is visible
+  const heroSection = await page.$eval('#home', (div) => div.id);
+  expect(heroSection).toBeTruthy();
+});
