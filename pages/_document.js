48c8a583f9f811c3513872a75b6f12fc9fe346ce
siddharth9805 <siddharth.u@somaiya.edu>
2023-12-21 18:03:50 -0500

    lighthouse error resolution

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Head from 'next/head';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" 
-          rel="stylesheet" 
-      />
       <Head />
       <title>Razor Edge</title>
       <body className='m-0'>
